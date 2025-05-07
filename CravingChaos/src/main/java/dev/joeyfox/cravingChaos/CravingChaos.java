1aad062743d376496f0c14c803ed6ef423888a00
Joey <james.lawrence@live.co.uk>
2025-05-07 16:37:55 +0100

    Update ;)

--- CravingChaos/src/main/java/dev/joeyfox/cravingChaos/CravingChaos.java
+++ CravingChaos/src/main/java/dev/joeyfox/cravingChaos/CravingChaos.java
@@ -6,7 +6,7 @@ public final class CravingChaos extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("CravingChaos plugin has been enabled!");
 
     }
 
