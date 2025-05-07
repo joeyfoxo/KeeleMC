7b8ddaf32e454ec72b7c20db29e8172ba57992c0
Joey <james.lawrence@live.co.uk>
2025-05-07 22:18:18 +0100

    Startup plugin.yml

--- CravingChaos/src/main/java/dev/joeyfox/cravingChaos/CravingChaos.java
+++ CravingChaos/src/main/java/dev/joeyfox/cravingChaos/CravingChaos.java
@@ -3,15 +3,18 @@ package dev.joeyfox.cravingChaos;
 import dev.joeyfox.cravingChaos.game.CravingGame;
 import dev.joeyfox.cravingChaos.game.CravingGameStart;
 import dev.joeyfoxo.core.Core;
+import org.bukkit.Bukkit;
 
 public final class CravingChaos extends Core<CravingGame> {
 
     @Override
     public void onEnable() {
         super.onEnable(); // This will call createGameInstance() inside Core
+        Bukkit.getScheduler().runTask(this, () -> {
         setKeeleMiniCore(this);
         new CravingGameStart(getGame());
         System.out.println("CravingChaos has been enabled!");
+    });
     }
 
     @Override
