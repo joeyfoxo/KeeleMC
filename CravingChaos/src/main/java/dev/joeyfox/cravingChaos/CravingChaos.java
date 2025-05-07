46878f424bf8721fa599f00d7bb2a2aacd9a574a
Joey <james.lawrence@live.co.uk>
2025-05-07 16:35:43 +0100

    Updating mini game core

---
+++ CravingChaos/src/main/java/dev/joeyfox/cravingChaos/CravingChaos.java
@@ -0,0 +1,17 @@
+package dev.joeyfox.cravingChaos;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CravingChaos extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
