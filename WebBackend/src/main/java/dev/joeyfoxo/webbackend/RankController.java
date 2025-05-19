7eaf083b5f4ecbc20a5c17737f3f70d0b84df9fd
Joey <james.lawrence@live.co.uk>
2025-05-19 17:17:14 +0100

    Backend

--- WebBackend/src/main/java/dev/joeyfoxo/webbackend/RankController.java
+++ WebBackend/src/main/java/dev/joeyfoxo/webbackend/RankController.java
@@ -1,11 +1,12 @@
 package dev.joeyfoxo.webbackend;
 
-import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/ranks")
