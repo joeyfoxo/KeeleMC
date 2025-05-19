5f3bcc0f51c7c623355c23f791c831588d5885e2
Joey <james.lawrence@live.co.uk>
2025-05-19 17:24:17 +0100

    Backend

--- WebBackend/src/main/java/dev/joeyfoxo/webbackend/WebBackendApplication.java
+++ WebBackend/src/main/java/dev/joeyfoxo/webbackend/WebBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RankController.class, args);
+        SpringApplication.run(WebBackendApplication.class, args);
     }
 
 }
