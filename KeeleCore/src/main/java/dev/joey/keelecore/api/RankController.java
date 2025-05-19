e50eabcb5acba2785727fe02578a2e0a957f2376
Joey <james.lawrence@live.co.uk>
2025-05-19 16:09:26 +0100

    Adding New Website Capabiltiies

--- KeeleCore/src/main/java/dev/joey/keelecore/api/RankController.java
+++ KeeleCore/src/main/java/dev/joey/keelecore/api/RankController.java
@@ -2,9 +2,11 @@ package dev.joey.keelecore.api;
 
 import dev.joey.keelecore.admin.permissions.PlayerRank;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
